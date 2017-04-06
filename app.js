b9cfc487512e84d44836d23d358166c07000cd2f
hengan <s19940703@gmail.com>
2017-04-06 21:42:42 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,37 @@
-var express = require('express');
-//var bodyParser = require('body-parser');
-var app = express();
+var express = require('express'),
+    app=express(),
+    path=require('path');
 
-//var urlencodedParser = bodyParser.urlencoded({ extended: false});
+var port=process.env.PORT || 3000;
 
-var port = process.env.PORT || 3000;
-
-app.set('view engine', 'ejs');
-
-//app.use('/public', express.static(__dirname + '/public'));
-
-app.use('/pic', express.static(__dirname + '/image'));
-app.use('/css', express.static(__dirname + '/css'));
-app.use('/js',express.static(__dirname + '/js_public'));
-
-app.use('/', function (req, res, next) {
-	console.log('Request Url:' + req.url);
-	next();
+app.use('/assets',express.static(__dirname+'/public'));
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use('/', function(req,res,next){
+    console.log('Request Url:' + req.url);
+    next();
 });
 
-app.get('/', function(req, res) {
-	res.render('index');
+app.get('/',function(req,res){
+    res.render('index');
 });
 
-app.get('/index.html', function(req, res) {
-	res.render('index');
+app.get('/index.html',function(req,res){
+    res.render('index');
 });
 
-app.get('/aboutme.html', function(req, res) {
-	res.render('aboutme');
+app.get('/aboutme.html',function(req,res){
+    res.render('aboutme');
 });
 
-app.get('/contact.html', function(req, res) {
-	res.render('contact');
+app.get('/contact.html',function(req,res){
+    res.render('contact');
 });
 
-app.get('/test.html', function(req, res) {
-	res.render('test');
-});
-
-app.get('/work.html', function(req, res) {
-	res.render('work');
+app.get('/works.html',function(req,res){
+    res.render('works');
 });
 
 
-app.post('/form', urlencodedParser, function(req,res){
-	res.send("Thank you");
-	console.log('Name is ' + req.body.name);
-	console.log('Age is ' + req.body.age);
-	console.log('Gender is ' + req.body.sex);
-	console.log('Most like ' + req.body.like);
-})
-
-app.get('*', function(req, res) {
-	res.render('err');
-});
 
 app.listen(port);
