005ad67133ed6f8761cadfd7b0aaa1c3f3e22e21
hengan <s19940703@gmail.com>
2017-04-06 21:24:17 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 var app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
+//var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-app.use('/pic', express.static(__dirname + '/pic'));
+//app.use('/public', express.static(__dirname + '/public'));
+
+app.use('/pic', express.static(__dirname + '/image'));
 app.use('/css', express.static(__dirname + '/css'));
+app.use('/js',express.static(__dirname + '/js_public'));
 
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
