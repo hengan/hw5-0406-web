51242486ef66d379d4e7baafac3bc345554447fe
hengan <s19940703@gmail.com>
2017-04-06 23:10:48 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,52 +1,24 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+
 var port = process.env.PORT || 3000;
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
 
-app.use(express.static(__dirname + '/public'));
+app.set('view engine','ejs');
 
-app.set('view engine', 'ejs');
+app.use('/css',express.static(__dirname + '/css'));
+app.use('/img',express.static(__dirname + '/imge'));
+app.use('/js',express.static(__dirname + '/js'));
 
-app.get('/', function(req, res) {
-	res.render('index');
+app.get('/', function( req , res ){
+    res.render('index');
 });
-
-app.get('/index.html', function(req, res) {
-	res.render('index');
+app.get('/aboutme', function( req , res ){
+    res.render('about');
 });
-
-app.get('/works.html', function(req, res) {
-	res.render('works');
-});
-
-app.get('/aboutme.html', function(req, res) {
-	res.render('aboutme');
-});
-
-app.get('/contact.html', function(req, res) {
-	res.render('contact');
-});
-
-
-app.get('*', function(req, res) {
-	res.render('err');
-});
-
-app.post('/home.html', urlencodedParser, function(req,res){
-	res.send("Thank you !");
-	console.log('Your firstname is ' + req.body.firstname);
-	console.log('Your lastname is ' + req.body.lastname);
-	console.log('Email is ' + req.body.email);
-	
-})
-
-app.listen(port);
-
-app.get('/works.html',function(req,res){
+app.get('/works', function( req , res ){
     res.render('works');
 });
-
-
-
+app.get('/contact', function( req , res ){
+    res.render('contact');
+});
 app.listen(port);
